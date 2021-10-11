d548ac89661ddd56f8252ab6e7820c87e1a08849
Steven Quintana <steve.e.quint@gmail.com>
2021-10-11 08:38:54 -0700

    Create headshot on left col of bio section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,13 +1,21 @@
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import headshot from '../../images/headshot.jpeg'
+import './AboutMe.css'
 
 const AboutMe = () => {
   return(
     <>
       <Container fluid="md" className="text-center">
         <Row>
-          <Col>1 of 2</Col>
+          <Col>
+            <img
+              src={headshot}
+              alt="Steven Quintana headshot"
+              className="headshot"
+            />
+          </Col>
           <Col>2 of 2</Col>
         </Row>
       </Container>
