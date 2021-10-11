b11e3a386cd75e172cea5aca39e466646538d1ea
Steven Quintana <steve.e.quint@gmail.com>
2021-10-11 08:33:28 -0700

    Set up grid for bio section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,9 +1,16 @@
-
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const AboutMe = () => {
   return(
     <>
-      
+      <Container fluid="md" className="text-center">
+        <Row>
+          <Col>1 of 2</Col>
+          <Col>2 of 2</Col>
+        </Row>
+      </Container>
     </>
   )
 }
