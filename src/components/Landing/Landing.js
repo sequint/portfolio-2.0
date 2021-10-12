8ae6a14007173ef568adb118684d532b64c88bb5
Steven Quintana <steve.e.quint@gmail.com>
2021-10-12 07:34:05 -0700

    Create an array of titles to be displayed and set an index state to zero

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,7 +1,29 @@
+import { useState } from 'react'
 import logo from '../../images/portfolio-logo.png'
 import './Landing.css'
 
+// Array for titles to display on the landing component.
+const titles = [
+  'Steven Quintana',
+  'Software Engineer',
+  'Full Stack Developer',
+  'React Developer',
+  'Problem Solver',
+  'Learner',
+  'Surfer'
+]
+
 const Landing = () => {
+  // Set state for the title.
+  const [ titleIndexState, setTitleIndexState ] = useState({
+    titleIndex: 0
+  })
+
+  // Set next title equal to the titles array at the state index.
+  const nexTitle = _ => {
+
+  }
+
   return(
     <div className="landing-container">
       <img 
