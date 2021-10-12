efd25e7a6081e071e40d6c5d3e5ebc830b8728be
Steven Quintana <steve.e.quint@gmail.com>
2021-10-12 09:36:56 -0700

    Delete bs files to start over with MUI

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,38 +1,18 @@
-import { useState, useEffect } from 'react'
 import logo from '../../images/portfolio-logo.png'
 import './Landing.css'
 
 // Array for titles to display on the landing component.
-const titles = [
-  'Steven Quintana',
-  'Software Engineer',
-  'Full Stack Developer',
-  'React Developer',
-  'Problem Solver',
-  'Learner',
-  'Surfer'
-]
+// const titles = [
+//   'Steven Quintana',
+//   'Software Engineer',
+//   'Full Stack Developer',
+//   'React Developer',
+//   'Problem Solver',
+//   'Learner',
+//   'Surfer'
+// ]
 
 const Landing = () => {
-  // Set state for the title.
-  const [ indexState, setIndexState ] = useState({
-    titleIndex: 0
-  })
-
-  useEffect(() => {
-    let currentIndex = indexState.titleIndex
-    setInterval(() => {
-      currentIndex === 6 ? setIndexState({ titleIndex: 0 }) : setIndexState({ titleIndex: currentIndex + 1 })
-    }, 3000)
-  })
-
-  // Set next title variable equal to the titles array at the state index.
-  let nextTitle = ''
-  nextTitle = titles[indexState.titleIndex]
-
-  const displayNextTitle = _ => {
-    return titles[index]
-  }
 
   return(
     <div className="landing-container">
@@ -41,7 +21,7 @@ const Landing = () => {
         className="landing-logo"
         alt="Steven Quintana logo"
       />
-      <h1 className="landing-title">{nextTitle}</h1>
+      <h1 className="landing-title">Steven Quintana</h1>
     </div>
   )
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,10 @@
 import Landing from '../../components/Landing/Landing'
-import NavBar from '../../components/NavBar/NavBar'
-import AboutMe from '../../components/AboutMe/AboutMe'
 import './Home.css'
 
 const Home = () => {
   return(
     <>
       <Landing />
-      <NavBar />
-      <AboutMe />
     </>
   )
 }
