aa20d3b7ec874ab0bdb222121abbbdb49a975f37
Steven Quintana <steve.e.quint@gmail.com>
2021-10-16 08:59:29 -0700

    Update about me using grid system and collapsing on xs screen

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,9 +1,23 @@
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import headshot from '../../images/headshot.jpeg'
+import './AboutMe.css'
+
+const aboutMeText = 'Full Stack Engineer leveraging 7 years of experience within the finance industry to work with a high performing team developing modern apps that disrupt the status quo and are easy to use. Known to be an independent problem solver and leader in fast-paced environments, with a history of leading teams to develop engaging applications.'
 
 const AboutMe = () => {
-  return(
-    <>
-      
-    </>
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <img src={headshot} class="headshot" alt="headshot" />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <h1>About Me</h1>
+          <p className="aboutMeText">{aboutMeText}</p>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
