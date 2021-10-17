51ddad972aad94c23b5a75b2bda568e9891d04c8
Steven Quintana <steve.e.quint@gmail.com>
2021-10-17 12:42:20 -0700

    Create a component to hold a timeline of the classes that have been taken

---
+++ src/components/ClassTimeline/ClassTimeline.js
@@ -0,0 +1,39 @@
+import Timeline from '@mui/lab/Timeline'
+import TimelineItem from '@mui/lab/TimelineItem'
+import TimelineSeparator from '@mui/lab/TimelineSeparator'
+import TimelineConnector from '@mui/lab/TimelineConnector'
+import TimelineContent from '@mui/lab/TimelineContent'
+import TimelineDot from '@mui/lab/TimelineDot'
+import './ClassTimeline.css'
+
+const ClassTimeline = () => {
+  return(
+    <Timeline>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>2021</TimelineContent>
+        <TimelineContent>Full Stack Bootcamp</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>2020</TimelineContent>
+        <TimelineContent>Data Structures</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+        </TimelineSeparator>
+        <TimelineContent>2013</TimelineContent>
+        <TimelineContent>Intro to C++</TimelineContent>
+      </TimelineItem>
+    </Timeline>
+  )
+}
+
+export default ClassTimeline
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,35 @@
 import NavBar from '../../components/NavBar/NavBar'
 import AboutMe from '../../components/AboutMe/AboutMe'
+import ClassTimeline from '../../components/ClassTimeline/ClassTimeline'
+// import Approach from '../../components/Approach/Approach'
+// import problemSolvingImg from '../../images/problem-solving.jpeg'
 // import Landing from '../../components/Landing/Landing'
+
+// const approaches = [
+//   {
+//     name: 'Problem Solving',
+//     description: 'Problem solving approach.',
+//     img: {problemSolvingImg}
+//   },
+//   {
+//     name: 'Comment First',
+//     description: 'Comment first approach.',
+//     img: { problemSolvingImg }
+//   },
+//   {
+//     name: 'debugging',
+//     description: 'Debugging approach.',
+//     img: { problemSolvingImg }
+//   }
+// ]
 
 const Home = () => {
   return(
     <>
       <NavBar />
       <AboutMe />
+      <ClassTimeline />
+      {/* {approaches.map(approach => <Approach approach={approach}/>)} */}
       {/* <Landing /> */}
     </>
   )
