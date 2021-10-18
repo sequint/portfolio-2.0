679f0313635f2e00b8b9e36ff0d74304d2e06be9
Steven Quintana <steve.e.quint@gmail.com>
2021-10-18 07:33:01 -0700

    Add title component to the projects page

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,9 +1,11 @@
 import NavBar from '../../components/NavBar/NavBar'
+import PageTitle from '../../components/PageTitle/PageTitle'
 
 const Projects = () => {
   return(
     <>
       <NavBar />
+      <PageTitle title="Projects" />
     </>
   )
 }
