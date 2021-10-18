5a27d3c2590869003469f64c74dc0f26dda0916f
Steven Quintana <steve.e.quint@gmail.com>
2021-10-18 08:28:12 -0700

    Import custom project images into the list

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -1,36 +1,28 @@
 import ImageList from '@mui/material/ImageList'
 import ImageListItem from '@mui/material/ImageListItem'
 import ImageListItemBar from '@mui/material/ImageListItemBar'
-import ListSubheader from '@mui/material/ListSubheader'
 import IconButton from '@mui/material/IconButton'
 import InfoIcon from '@mui/icons-material/Info'
+import pubImg from '../../images/project-screenshots/PUB-icon.png'
+import sessionImg from '../../images/project-screenshots/session-icon.png'
+import binaryTreeImg from '../../images/project-screenshots/binary-tree-icon.png'
 
 const itemData = [
   {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+    img: pubImg,
     title: 'Breakfast',
     author: '@bkristastucchio',
     featured: true,
   },
   {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+    img: sessionImg,
     title: 'Burger',
     author: '@rollelflex_graphy726',
   },
   {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    img: binaryTreeImg,
     title: 'Camera',
     author: '@helloimnik',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-    author: '@nolanissac',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-    author: '@hjrc33',
   }
 ]
 
