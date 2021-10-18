6b69f1865ed5878abf496522dd0db3a38354a027
Steven Quintana <steve.e.quint@gmail.com>
2021-10-17 21:02:27 -0700

    Create an input form that appears and disappears on click

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -1,43 +1,52 @@
 import { useState } from 'react'
 import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab';
-import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import NavigationIcon from '@mui/icons-material/Navigation';
 import TextField from '@mui/material/TextField';
-import InsertCommentIcon from '@mui/icons-material/InsertComment';
 import InsertComment from '@mui/icons-material/InsertComment';
 import './ContactMe.css'
 
-const actions = [
-  {
-    icon: <TextField
-            required
-            id="outlined-required"
-            label="Required"
-            defaultValue="Hello World"
-          />,
-    name: 'Email'
-  },
-  {
-    icon: <TextField />, name: 'Email'
-  }
-]
-
 const ContactMe = () => {
-  const [value, setValue] = useState('Controlled')
+  const [ click, toggleClick ] = useState(false)
 
-  const handleChange = (event) => {
-    setValue(event.target.value)
+  const handleClick = event => {
+    event.preventDefault()
+    toggleClick(!click)
   }
 
-  const handleClick = () => {
-    console.log('click')
+  const displayInputArea = () => {
+
+    return(
+      <>
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1},
+          }}
+          noValidate
+          autoComplete="off"
+          className="messageInput"
+        >
+          <div>
+            <TextField
+              id="outlined-email"
+              label="Email"
+            />
+            <TextField
+              id="outlined-multiline-static"
+              label="Message"
+              multiline
+              rows={4}
+            />
+          </div>
+        </Box>
+      </>
+    )
+
   }
 
   return(
     <>
+      {click ? displayInputArea() : <></>}
       <Box sx={{ position: 'sticky', bottom: 0, float: 'right', padding: '0px 10px 10px 0px', transform: 'translateZ(0px)', flexGrow: 1 }}>
         <Fab className="contactBtnColor" aria-label="add" onClick={handleClick}>
           <InsertComment />
