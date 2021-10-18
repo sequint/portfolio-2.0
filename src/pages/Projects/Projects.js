2f5f5a1b4b49476976fd27d310a7e57e731040ec
Steven Quintana <steve.e.quint@gmail.com>
2021-10-18 08:16:39 -0700

    Create an image list to display projects in projectlist comp

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,11 +1,13 @@
 import NavBar from '../../components/NavBar/NavBar'
 import PageTitle from '../../components/PageTitle/PageTitle'
+import ProjectList from '../../components/ProjectList/ProjectList'
 
 const Projects = () => {
   return(
     <>
       <NavBar />
       <PageTitle title="Projects" />
+      <ProjectList />
     </>
   )
 }
