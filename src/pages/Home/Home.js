fcb234f75ea556c19478d165f9168ff78982bde6
Steven Quintana <steve.e.quint@gmail.com>
2021-10-17 21:41:45 -0700

    Delete old componenets from home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,27 +2,7 @@ import NavBar from '../../components/NavBar/NavBar'
 import AboutMe from '../../components/AboutMe/AboutMe'
 import ClassTimeline from '../../components/ClassTimeline/ClassTimeline'
 import ContactMe from '../../components/ContactMe/ContactMe'
-// import Approach from '../../components/Approach/Approach'
-// import problemSolvingImg from '../../images/problem-solving.jpeg'
 // import Landing from '../../components/Landing/Landing'
-
-// const approaches = [
-//   {
-//     name: 'Problem Solving',
-//     description: 'Problem solving approach.',
-//     img: {problemSolvingImg}
-//   },
-//   {
-//     name: 'Comment First',
-//     description: 'Comment first approach.',
-//     img: { problemSolvingImg }
-//   },
-//   {
-//     name: 'debugging',
-//     description: 'Debugging approach.',
-//     img: { problemSolvingImg }
-//   }
-// ]
 
 const Home = () => {
   return(
@@ -31,7 +11,6 @@ const Home = () => {
       <AboutMe />
       <ClassTimeline />
       <ContactMe />
-      {/* {approaches.map(approach => <Approach approach={approach}/>)} */}
       {/* <Landing /> */}
     </>
   )
