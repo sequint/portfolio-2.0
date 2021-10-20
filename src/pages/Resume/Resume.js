fd8dc095d08bd747c0e41699344f25e3ab6a9b8b
Steven Quintana <steve.e.quint@gmail.com>
2021-10-20 09:04:20 -0700

    Create the resume page with embedded google drive pdf, make pdf height and width spance the page

---
+++ src/pages/Resume/Resume.js
@@ -0,0 +1,13 @@
+import NavBar from "../../components/NavBar/NavBar"
+import './Resume.css'
+
+const Resume = () => {
+  return(
+    <div className="resumePageContainer">
+      <NavBar />
+      <iframe src="https://drive.google.com/file/d/1A-tr3On9p74ALBTIYh_DQC__yvqADgqM/preview" className="resumePDF" title="resume" allow="autoplay"></iframe>
+    </div>
+  )
+}
+
+export default Resume
