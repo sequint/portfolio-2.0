2d4f407816217cc5a7335f208784f20cf569f518
Steven Quintana <steve.e.quint@gmail.com>
2021-10-26 09:59:26 -0700

    Replace text in timeline with a card that display class title and description

---
+++ src/components/ClassCard/ClassCard.js
@@ -0,0 +1,27 @@
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const ClassCard = ({ classTitle, classDescription }) => {
+
+  return (
+    <Card sx={{ minWidth: 275, maxWidth: 400 }}>
+      <CardContent>
+        <Typography variant="h5" component="div">
+          {classTitle}
+        </Typography>
+        <Typography variant="body2">
+          {classDescription}
+          <br />
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  )
+}
+
+export default ClassCard
--- src/components/ClassTimeline/ClassTimeline.js
+++ src/components/ClassTimeline/ClassTimeline.js
@@ -4,36 +4,48 @@ import TimelineSeparator from '@mui/lab/TimelineSeparator'
 import TimelineConnector from '@mui/lab/TimelineConnector'
 import TimelineContent from '@mui/lab/TimelineContent'
 import TimelineDot from '@mui/lab/TimelineDot'
+import ClassCard from '../ClassCard/ClassCard'
 import './ClassTimeline.css'
+
+const classes = [
+  {
+    year: 2021,
+    title: 'Full Stack Bootcamp',
+    description: 'Learned to develop full stack progress web applications focused on MERN Stack development. Was team lead on three projects culminating in a final fully deployed commercial use application.'
+  },
+  {
+    year: 2020,
+    title: 'Data Structures',
+    description: 'Learned basic data structures in C++ language ranging from linked lists, ques, and stacks to binary trees using nodes to search and sort. Was a top performer in the class, recieving an A+ on all projects, and leading the design of a final binary tree design.'
+  },
+  {
+    year: 2013,
+    title: 'Intro to Programming: C++',
+    description: 'Learned basic functional and object oriented programming in the C++ language. Top performer in the class creating functional console applications with a focus on effecient memory allocation and reusable code.'
+  }
+]
 
 const ClassTimeline = () => {
   return(
     <>
       <h1 className="timelineTitle">Classes</h1>
       <Timeline>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>2021</TimelineContent>
-          <TimelineContent>Full Stack Bootcamp</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>2020</TimelineContent>
-          <TimelineContent>Data Structures</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-          </TimelineSeparator>
-          <TimelineContent>2013</TimelineContent>
-          <TimelineContent>Intro to C++</TimelineContent>
-        </TimelineItem>
+        {classes.map(oneClass => {
+          return(
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent>
+                {oneClass.year}
+                <br/><br/>
+                <ClassCard classTitle={oneClass.title} classDescription={oneClass.description} />
+                <br /><br />
+              </TimelineContent>
+            </TimelineItem>
+          )
+        })}
       </Timeline>
     </>
   )
