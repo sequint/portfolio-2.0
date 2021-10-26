3ffae38aa6636f5f1338cd749142ac789a60ffed
Steven Quintana <steve.e.quint@gmail.com>
2021-10-26 13:27:41 -0700

    Remove last title component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,6 @@ import NavBar from '../../components/NavBar/NavBar'
 import AboutMe from '../../components/AboutMe/AboutMe'
 import ClassTimeline from '../../components/ClassTimeline/ClassTimeline'
 import ContactMe from '../../components/ContactMe/ContactMe'
-import PageTitle from '../../components/PageTitle/PageTitle'
 import SkillList from '../../components/SkillList/SkillList'
 import ProjectList from '../../components/ProjectList/ProjectList'
 // import Landing from '../../components/Landing/Landing'
@@ -14,7 +13,6 @@ const Home = () => {
       <AboutMe />
       <ClassTimeline />
       <SkillList />
-      <PageTitle title="Projects" />
       <ProjectList />
       <ContactMe />
       {/* <Landing /> */}
