69618a351613ef1f3ad5f5b29c9dc15ff38b67de
Steven Quintana <steve.e.quint@gmail.com>
2021-10-27 15:55:34 -0700

    Try getting rid of custom style

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,13 +1,14 @@
 import NavBar from "../../components/NavBar/NavBar"
-import resume from '../../assets/resume.pdf'
+// import resume from '../../assets/resume.pdf'
 import './Resume.css'
 
 const Resume = () => {
   return(
     <>
-      {/* <NavBar /> */}
+      <NavBar />
 
-        <embed src={resume} className="resumePDF" type="application/pdf" width="100%" height="100%" title="resume"></embed>
+      {/* <embed src="https://drive.google.com/file/d/1A-tr3On9p74ALBTIYh_DQC__yvqADgqM/view?usp=sharing" className="resumePDF" type="application/pdf" width="100%" height="100%" title="resume"></embed> */}
+      <iframe src="https://drive.google.com/file/d/1A-tr3On9p74ALBTIYh_DQC__yvqADgqM/preview" width="900" height="700" title="resume" allow="autoplay"></iframe>
 
     </>
   )
