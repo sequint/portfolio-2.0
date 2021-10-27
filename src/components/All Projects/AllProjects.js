9d7a92db871d38920a1374d32dc9ab88593de018
Steven Quintana <steve.e.quint@gmail.com>
2021-10-27 11:41:55 -0700

    Update project page to hold all projects

---
+++ src/components/All Projects/AllProjects.js
@@ -0,0 +1,155 @@
+import { useState, useEffect } from 'react'
+import ImageList from '@mui/material/ImageList'
+import ImageListItem from '@mui/material/ImageListItem'
+import ImageListItemBar from '@mui/material/ImageListItemBar'
+import IconButton from '@mui/material/IconButton'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LanguageIcon from '@mui/icons-material/Language'
+import PageTitle from '../../components/PageTitle/PageTitle'
+import pubImg from '../../images/project-screenshots/PUB-icon.png'
+import sessionImg from '../../images/project-screenshots/session-icon.png'
+import joferImg from '../../images/project-screenshots/JOFER.png'
+import './AllProjects.css'
+
+const projects = [
+  {
+    img: joferImg,
+    title: 'Jofer',
+    author: '#React.js #Node.js #MongoDB',
+    featured: true,
+    gitHubLink: 'https://github.com/sequint/Jofer',
+    deployedSite: 'https://jofer.herokuapp.com',
+    description: `An app that allows users to create and manage projects that they are working on, while tracking other projects that they like.`
+  },
+  {
+    img: pubImg,
+    title: 'PUB',
+    author: '#Node.js #FullStack',
+    gitHubLink: 'https://github.com/sequint/pub',
+    deployedSite: 'https://pure-brook-86019.herokuapp.com/',
+    description: `Stands for 'Project Hub'. This app allow you to create and update progress on projects you are working on. It also allows other users to track and comment on your projects to help you out!`
+  },
+  {
+    img: sessionImg,
+    title: 'Session',
+    author: '#API #VanillaJS',
+    gitHubLink: 'https://github.com/sequint/session',
+    deployedSite: 'https://sequint.github.io/session/',
+    description: `Stands for 'Project Hub'. This app allow you to create and update progress on projects you are working on. It also allows other users to track and comment on your projects to help you out!`
+  }
+]
+
+const AllProjects = () => {
+  const [size, setSize] = useState(window.innerWidth)
+  const [isXS, setIsXS] = useState(true)
+
+  const updateWidth = _ => {
+    setSize(window.innerWidth)
+  }
+
+  useEffect(() => {
+
+    window.addEventListener("resize", updateWidth)
+
+    if (size > 375) {
+      console.log('in large')
+      setIsXS(false)
+    }
+    else if (size <= 375) {
+      console.log('in xs')
+      setIsXS(true)
+    }
+
+    return () => window.removeEventListener("resize", updateWidth)
+
+  }, [size])
+
+  const renderDynamicList = _ => {
+    if (isXS) {
+      console.log('xs screen')
+      return (
+        <ImageList
+          className="projects"
+          sx={{ width: 300, height: 600, margin: '20px auto 0px auto' }}
+          cols={1}
+        >
+          {projects.map((project) => (
+            <ImageListItem key={project.img}>
+              <img
+                src={`${project.img}?w=248&fit=crop&auto=format`}
+                srcSet={`${project.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                alt={project.title}
+                loading="lazy"
+              />
+              <ImageListItemBar
+                title={project.title}
+                subtitle={project.author}
+                actionIcon={
+                  <IconButton
+                    sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                    aria-label={`info about ${project.title}`}
+                  >
+                    <a className="noDecor" href={project.deployedSite} target="_blank" rel="noreferrer">
+                      <LanguageIcon className="linkIcon" sx={{ fill: 'white' }} />
+                    </a>
+                    <a className="noDecor" href={project.gitHubLink} target="_blank" rel="noreferrer">
+                      <GitHubIcon className="linkIcon" sx={{ fill: 'white' }} />
+                    </a>
+                  </IconButton>
+                }
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      )
+    }
+    else {
+      console.log('bigger screen')
+      return (
+        <ImageList ImageList className="imgListContainer" >
+          {
+            projects.map(project => (
+              <ImageListItem key={project.img}>
+                <img
+                  src={`${project.img}?w=248&fit=crop&auto=format`}
+                  srcSet={`${project.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                  alt={project.title}
+                  loading="lazy"
+                />
+                <ImageListItemBar
+                  title={project.title}
+                  subtitle={project.author}
+                  actionIcon={
+                    <IconButton
+                      className="linkIcon"
+                      sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                      aria-label={`info about ${project.title}`}
+                    >
+                      <a className="noDecor linkIcon" href={project.deployedSite} target="_blank" rel="noreferrer">
+                        <LanguageIcon className="linkIcon" sx={{ fill: 'white' }} />
+                      </a>
+                      <a className="noDecor" href={project.gitHubLink} target="_blank" rel="noreferrer">
+                        <GitHubIcon className="linkIcon" sx={{ fill: 'white' }} />
+                      </a>
+                    </IconButton>
+                  }
+                />
+              </ImageListItem>
+            ))
+          }
+        </ImageList >
+      )
+    }
+  }
+
+  return(
+    <>
+      <PageTitle title="Projects" />
+      <div className="projectContainer">
+        {renderDynamicList()}
+      </div>
+    </>
+  )
+}
+
+export default AllProjects
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,14 +1,12 @@
 import NavBar from '../../components/NavBar/NavBar'
-import PageTitle from '../../components/PageTitle/PageTitle'
-import ProjectList from '../../components/ProjectList/ProjectList'
+import AllProjects from '../../components/All Projects/AllProjects'
 import ContactMe from '../../components/ContactMe/ContactMe'
 
 const Projects = () => {
   return(
     <>
       <NavBar />
-      <PageTitle title="Projects" />
-      <ProjectList />
+      <AllProjects />
       <ContactMe />
     </>
   )
