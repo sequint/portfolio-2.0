2eacced28fd81fd5e7f23884244f11f591065c7a
Steven Quintana <steve.e.quint@gmail.com>
2021-10-27 14:48:27 -0700

    Create div around embeded with style in it

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -6,9 +6,9 @@ const Resume = () => {
   return(
     <div className="resumePageContainer">
       <NavBar />
-      <wrapper>
-        <embed src="https://drive.google.com/file/d/1A-tr3On9p74ALBTIYh_DQC__yvqADgqM/preview" width="100%" height="100%" className="resumePDF" title="resume"></embed>
-      </wrapper>
+      <div className="resumePDF">
+        <embed src="https://drive.google.com/file/d/1A-tr3On9p74ALBTIYh_DQC__yvqADgqM/preview" width="100%" height="100%" title="resume"></embed>
+      </div>
     </div>
   )
 }
