26ec17907c6a30ca921186e59f3b72120b7b74fb
Steven Quintana <steve.e.quint@gmail.com>
2021-10-27 12:48:12 -0700

    Update style inline to fix bug on deployed app

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -5,7 +5,7 @@ const Resume = () => {
   return(
     <div className="resumePageContainer">
       <NavBar />
-      <iframe src="https://drive.google.com/file/d/1A-tr3On9p74ALBTIYh_DQC__yvqADgqM/preview" className="resumePDF" title="resume" allow="autoplay"></iframe>
+      <iframe src="https://drive.google.com/file/d/1A-tr3On9p74ALBTIYh_DQC__yvqADgqM/preview" width="100%" height="100%" className="resumePDF" title="resume" allow="autoplay"></iframe>
     </div>
   )
 }
