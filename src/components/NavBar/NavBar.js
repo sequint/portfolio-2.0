5c24a6b849c9bdbcc694aadcf4fdcc4100de78bd
Steven Quintana <steve.e.quint@gmail.com>
2021-10-26 17:08:33 -0700

    Make resume button content dynamic with screen size

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,24 +1,24 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import HomeIcon from '@mui/icons-material/Home';
-import CodeIcon from '@mui/icons-material/Code';
-import ArchitectureIcon from '@mui/icons-material/Architecture';
-import AttachFileIcon from '@mui/icons-material/AttachFile';
+import { styled, useTheme } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import Drawer from '@mui/material/Drawer'
+import MuiAppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import List from '@mui/material/List'
+import Typography from '@mui/material/Typography'
+import Divider from '@mui/material/Divider'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import ListItem from '@mui/material/ListItem'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import HomeIcon from '@mui/icons-material/Home'
+import CodeIcon from '@mui/icons-material/Code'
+import ArchitectureIcon from '@mui/icons-material/Architecture'
+import AttachFileIcon from '@mui/icons-material/AttachFile'
 import ResumeButton from '../ResumeButton/ResumeButton'
 import logo from '../../images/portfolio-logo.png'
 import './NavBar.css'
--- src/components/ResumeButton/ResumeButton.js
+++ src/components/ResumeButton/ResumeButton.js
@@ -1,6 +1,31 @@
+import { useState, useEffect } from 'react'
+import AttachFileIcon from '@mui/icons-material/AttachFile'
 import './ResumeButton.css'
 
 const ResumeButton = () => {
+  const [size, setSize] = useState(window.innerWidth)
+  const [isXS, setIsXS] = useState(true)
+
+  const updateWidth = _ => {
+    setSize(window.innerWidth)
+  }
+
+  useEffect(() => {
+
+    window.addEventListener("resize", updateWidth)
+
+    if (size > 375) {
+      console.log('in large')
+      setIsXS(false)
+    }
+    else if (size <= 375) {
+      console.log('in xs')
+      setIsXS(true)
+    }
+
+    return () => window.removeEventListener("resize", updateWidth)
+
+  }, [size])
 
   const handleResumeClick = event => {
     event.preventDefault()
@@ -9,7 +34,7 @@ const ResumeButton = () => {
 
   return(
     <>
-      <button className="resumeBtn" onClick={handleResumeClick}>Resume</button>
+      <button className="resumeBtn" onClick={handleResumeClick}>{isXS ? <AttachFileIcon /> : 'Resume'}</button>
     </>
   )
 }
