1d5ae0d536a690a76ee66bbf8ff554465b58e08c
Steven Quintana <steve.e.quint@gmail.com>
2021-10-27 14:53:58 -0700

    Change back to local pdf file

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,5 +1,5 @@
 import NavBar from "../../components/NavBar/NavBar"
-// import resume from '../../assets/resume.pdf'
+import resume from '../../assets/resume.pdf'
 import './Resume.css'
 
 const Resume = () => {
@@ -7,7 +7,7 @@ const Resume = () => {
     <div className="resumePageContainer">
       <NavBar />
       <div className="resumePDF">
-        <embed src="https://drive.google.com/file/d/1A-tr3On9p74ALBTIYh_DQC__yvqADgqM/preview" width="100%" height="100%" title="resume"></embed>
+        <embed src={resume} width="100%" height="100%" title="resume"></embed>
       </div>
     </div>
   )
