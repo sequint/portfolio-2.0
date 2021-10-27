0a2497fb9d4c92a230c897df5c5d86d38d73b1a8
Steven Quintana <steve.e.quint@gmail.com>
2021-10-27 15:17:56 -0700

    Remove old style

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -4,12 +4,12 @@ import './Resume.css'
 
 const Resume = () => {
   return(
-    <div className="resumePageContainer">
+    <>
       <NavBar />
-      <div className="resumePDF">
-        <embed src={resume} width="100%" height="100%" title="resume"></embed>
+      <div>
+        <embed src={resume} className="resumePDF" width="100%" height="100%" title="resume"></embed>
       </div>
-    </div>
+    </>
   )
 }
 
