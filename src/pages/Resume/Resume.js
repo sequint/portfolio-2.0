2e26af4472cb71f2a253155fa4168cc6913faa05
Steven Quintana <steve.e.quint@gmail.com>
2021-10-27 14:17:13 -0700

    Update pdf path to a local file to fix deployment bug

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,11 +1,12 @@
 import NavBar from "../../components/NavBar/NavBar"
+import resume from '../../assets/resume.pdf'
 import './Resume.css'
 
 const Resume = () => {
   return(
     <div className="resumePageContainer">
       <NavBar />
-      <iframe src="https://drive.google.com/file/d/1A-tr3On9p74ALBTIYh_DQC__yvqADgqM/preview" width="100%" height="100%" className="resumePDF" title="resume" allow="autoplay"></iframe>
+      <iframe src={resume} width="100%" height="100%" className="resumePDF" title="resume" allow="autoplay"></iframe>
     </div>
   )
 }
