7056c3aa94f46ef188fce0dc23e3ec3030d5f300
Steven Quintana <steve.e.quint@gmail.com>
2021-10-28 09:14:12 -0700

    Update resume status on mobile

--- src/components/ResumeButton/ResumeButton.js
+++ src/components/ResumeButton/ResumeButton.js
@@ -1,42 +1,11 @@
-import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import AttachFileIcon from '@mui/icons-material/AttachFile'
 import './ResumeButton.css'
 
 const ResumeButton = () => {
-  const [size, setSize] = useState(window.innerWidth)
-  const [isXS, setIsXS] = useState(true)
-
-  const updateWidth = _ => {
-    setSize(window.innerWidth)
-  }
-
-  useEffect(() => {
-
-    window.addEventListener("resize", updateWidth)
-
-    if (size > 375) {
-      console.log('in large')
-      setIsXS(false)
-    }
-    else if (size <= 375) {
-      console.log('in xs')
-      setIsXS(true)
-    }
-
-    return () => window.removeEventListener("resize", updateWidth)
-
-  }, [size])
-
-  const handleResumeClick = event => {
-    event.preventDefault()
-    window.location = './resume'
-  }
-
   return(
     <>
       <Link to="/resume">
-        <button className="resumeBtn">{isXS ? <AttachFileIcon /> : 'Resume'}</button>
+        <button className="resumeBtn">Resume</button>
       </Link>
     </>
   )
--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,5 +1,5 @@
 import NavBar from "../../components/NavBar/NavBar"
-import resume from '../../assets/resume.pdf'
+import sadFace from '../../images/sad-face.png'
 import './Resume.css'
 
 const Resume = () => {
@@ -7,6 +7,7 @@ const Resume = () => {
     <>
       <NavBar />
       <embed src="https://drive.google.com/file/d/1A-tr3On9p74ALBTIYh_DQC__yvqADgqM/preview" className="resumePDF" type="application/pdf" width="100%" height="100%" title="resume"></embed>
+      <img src={sadFace} alt="sad face" className="sadFace" />
     </>
   )
 }
