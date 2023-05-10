aa6b91b217c600259f8363f0015d9afdc48fad7c
stevenquintana <steve.e.quint@gmail.com>
2023-05-10 07:17:45 -0700

    Update src path with local resume pdf

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,12 +1,13 @@
 import NavBar from "../../components/NavBar/NavBar"
 import sadFace from '../../images/sad-face.png'
+import resumePDF from '../../assets/resume'
 import './Resume.css'
 
 const Resume = () => {
   return(
     <>
       <NavBar />
-      <embed src="https://drive.google.com/drive/u/0/search?ths=true&q=steven%27s%20resu" className="resumePDF" type="application/pdf" width="100%" height="100%" title="resume"></embed>
+      <embed src={resumePDF} className="resumePDF" type="application/pdf" width="100%" height="100%" title="resume"></embed>
       <img src={sadFace} alt="sad face" className="sadFace" />
     </>
   )
